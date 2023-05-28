16dc37d7ebad619ac5c97c9439d0d4fd7d15c457
Andres Clavijo~ <shipoca2012@hotmail.com>
2023-05-28 13:02:07 -0500

    seccion 12 clase 162 Se añade opcion para obtener url imagen, se añaden funciones de cargue de archivos.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }), 
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -19,6 +23,7 @@ import { FilesModule } from './files/files.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
+
     ProductsModule,
     CommonModule,
     SeedModule,
--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,8 +1,75 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FilesService } from './files.service';
+import { AnyFilesInterceptor, FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/file-filter';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/file-namer';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configServie: ConfigService,
+  ) {}
 
+  @Get('product/:imageName')
+  findFileProduct(@Res() res: Response, @Param('imageName') imageName: string) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadFile(
+    @UploadedFile()
+    file: Express.Multer.File,
+  ) {
+    console.log(file);
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+    const secureUrl = `${this.configServie.get('HOST_API')}/files/product/${file.filename}`;
+
+    return { secureUrl };
+  }
+
+  @Post('multiple')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadFiles(
+    @UploadedFiles()
+    files: Array<Express.Multer.File>,
+  ) {
+    if (!files || files.length == 0) {
+      throw new BadRequestException('Make sure that the files is an image');
+    }
+    return { fileName: files.map((file) => `${this.configServie.get('HOST_API')}/files/product/${file.filename}`) };
+  }
 }
